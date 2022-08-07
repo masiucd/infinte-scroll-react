5d1103a37d82798deccc4baf4a8b9342635fbca1
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-08-07 19:21:57 +0200

    Starter

---
+++ src/components/test.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let count: number = 0;
+</script>
+
+<h1>count is {count}</h1>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Test from "../components/test.svelte";
+
+	let name = "Marcell";
+	let isOn = false;
+	const toggleName = () => {
+		isOn = !isOn;
+		return isOn ? (name = "Marcell") : (name = "Marcello");
+	};
+</script>
+
+<h1>Task book built with Svelete and Svelte-kit</h1>
+<p>{name}</p>
+<button on:click={toggleName}>Toggle</button>
+<Test count={100} />
