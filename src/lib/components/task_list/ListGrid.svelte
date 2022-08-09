4deced7227f7ec8b4afbadbfb28c8dfddd9a76e3
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-08-09 07:45:51 +0200

    ListGrid component

---
+++ src/lib/components/task_list/ListGrid.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type {Task} from "$lib/util/task"
+	import TaskList from "./TaskList.svelte"
+	export let uncompletedTasks: Task[]
+	export let completedTasks: Task[]
+</script>
+
+<div class="grid grid-cols-2 h-full items-center gap-4">
+	<div class=" min-h-[10rem] max-h-[25rem]">
+		<h4 class="mb-1 text-2xl">Uncompled</h4>
+		<TaskList tasks={uncompletedTasks} type="unompleted" />
+	</div>
+	<div class="-500 min-h-[10rem] max-h-[25rem]">
+		<h4 class="mb-1 text-2xl">Completed</h4>
+		<TaskList tasks={completedTasks} type="completed" />
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,15 +3,13 @@
 
 <script lang="ts">
 	import AddTaskForm from "$lib/components/task_list/AddTaskForm.svelte"
-	import TaskList from "$lib/components/task_list/TaskList.svelte"
+	import ListGrid from "$lib/components/task_list/ListGrid.svelte"
 	import Page from "$lib/Page.svelte"
 	import {genRandomString} from "$lib/util/helpers"
 	import {staticTasks as tasks, TaskStatus} from "$lib/util/task"
 
 	let uncompletedTasks = tasks.filter(({done}) => !done)
 	let completedTasks = tasks.filter(({done}) => done)
-	let title: string
-	let priority: TaskStatus
 
 	const addTodo = (title: string, priority: TaskStatus) => {
 		uncompletedTasks = [
@@ -34,15 +32,6 @@
 <Page>
 	<div class="h-[43.75rem] min-h-[30rem] p-1">
 		<AddTaskForm {addTodo} />
-		<div class="grid grid-cols-2 h-full items-center gap-4">
-			<div class=" min-h-[10rem] max-h-[25rem]">
-				<h4 class="mb-1 text-2xl">Uncompled</h4>
-				<TaskList tasks={uncompletedTasks} type="unompleted" />
-			</div>
-			<div class="-500 min-h-[10rem] max-h-[25rem]">
-				<h4 class="mb-1 text-2xl">Completed</h4>
-				<TaskList tasks={completedTasks} type="completed" />
-			</div>
-		</div>
+		<ListGrid {uncompletedTasks} {completedTasks} />
 	</div>
 </Page>
