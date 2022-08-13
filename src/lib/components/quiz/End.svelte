b51219ad246e87f00d254b7d3115ea05fa210615
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-08-13 15:44:57 +0200

    Quiz game

---
+++ src/lib/components/quiz/End.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let score: number
+	export let amountOfQuestions: number
+	export let resetGame: () => void
+</script>
+
+<h3>Game is finished</h3>
+<p class="mb-2">
+	You
+	<span
+		class="relative z-10 inline-block after:content-[''] after:absolute after:bottom-1 after:left-0 after:h-2 after:w-full after:bg-orange-500 after:opacity-70 -after:translate-x-7  after:rotate-2 after:-z-10"
+	>
+		scored
+		{score}
+	</span>/{amountOfQuestions} possible questions
+</p>
+<button on:click={resetGame}>New game</button>
--- src/routes/quiz/index.svelte
+++ src/routes/quiz/index.svelte
@@ -3,9 +3,12 @@
 	import Title from "$lib/components/Title.svelte"
 	import ButtonDefault from "$lib/components/common/ButtonDefault.svelte"
 	import {questions} from "$lib/util/quiz"
+	import End from "$lib/components/quiz/End.svelte"
 
+	let amountOfQuestions = questions.length
 	$: quizIndex = 0
 	$: score = 0
+	$: enabled = () => quizIndex !== questions.length
 </script>
 
 <svelte:head>
@@ -15,24 +18,25 @@
 
 <Page>
 	<Title text="Quiz game" />
-
 	<div>
-		<div class="rounded-md my-5 p-2 bg-orange-100 shadow flex flex-col w-3/5 ">
+		<div
+			class="rounded-md my-5 p-2 bg-orange-100 shadow flex flex-col w-4/5 sm:w-3/5"
+		>
 			<h4 class="mb-2">
 				{questions[
-					quizIndex >= questions.length ? questions.length - 1 : quizIndex
+					quizIndex >= amountOfQuestions ? amountOfQuestions - 1 : quizIndex
 				].question.q}
 			</h4>
 			<ul
 				class="grid grid-cols-2 max-w-md place-items-center p-2 gap-2 shadow bg-blue-600 rounded-md"
 			>
-				{#each questions[quizIndex < questions.length ? quizIndex : quizIndex - 1].options as item}
+				{#each questions[quizIndex < amountOfQuestions ? quizIndex : quizIndex - 1].options as item}
 					<li>
 						<ButtonDefault
-							disabled={quizIndex === questions.length}
+							disabled={!enabled()}
 							styles="text-xl text-white"
 							onClick={() => {
-								if (quizIndex < questions.length) {
+								if (quizIndex < amountOfQuestions) {
 									if (item === questions[quizIndex].question.a) {
 										score += 1
 									}
@@ -47,22 +51,14 @@
 			</ul>
 		</div>
 
-		{#if quizIndex === questions.length}
-			<h3>Game is finished</h3>
-			<p class="mb-2">
-				You
-				<span
-					class="relative z-10 inline-block after:content-[''] after:absolute after:bottom-1 after:left-0 after:h-2 after:w-full after:bg-orange-500 after:opacity-70 -after:translate-x-7  after:rotate-2 after:-z-10"
-				>
-					scored
-					{score}
-				</span>/{questions.length} possible questions
-			</p>
-			<button
-				on:click={() => {
+		{#if quizIndex === amountOfQuestions}
+			<End
+				{score}
+				{amountOfQuestions}
+				resetGame={() => {
 					quizIndex = 0
-				}}>New game</button
-			>
+				}}
+			/>
 		{/if}
 	</div>
 </Page>
