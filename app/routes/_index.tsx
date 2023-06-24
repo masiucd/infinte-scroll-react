11eed2035aab7c079c714d6516893f4837301495
masiucd <ciszekmarcell@gmail.com>
2023-06-24 17:33:44 +0200

    Static content

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,11 +2,54 @@ import type {V2_MetaFunction} from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    {title: "New Remix App"},
-    {name: "description", content: "Welcome to Remix!"},
+    {title: "My working journal"},
+    {
+      name: "description",
+      content: "My working journal is a place where I write about my work..",
+    },
   ];
 };
 
 export default function Index() {
-  return <h1 className="m-10  bg-red-400 p-10 text-blue-400 ">Hello</h1>;
+  return (
+    <div className="p-10">
+      <h1 className="mb-2 text-5xl font-bold">Work journal</h1>
+      <p className="mb-4 text-lg text-gray-300">
+        Learnings and thoughts about my work as a software developer. Updated
+        weekly.
+      </p>
+
+      <section className="flex flex-col gap-2 p-1">
+        <p className="mb-2 font-bold">
+          Week of July 19<sup>th</sup>, 2023
+        </p>
+
+        <div className="flex flex-col gap-4">
+          <div>
+            <p>Work</p>
+            <ul className="ml-10 list-disc">
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+
+          <div>
+            <p>Learnings</p>
+            <ul className="ml-10 list-disc">
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+
+          <div>
+            <p>Thoughts</p>
+            <ul className="ml-10 list-disc">
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,15 @@
 import type {Config} from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // To just have on nuance of the gray color
+        gray: colors.neutral,
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
