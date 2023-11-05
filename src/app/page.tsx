960179818d95edb41a638d3004091d57737cc3ce
masiucd <ciszekmarcell@gmail.com>
2023-11-05 12:28:34 +0100

    Reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
 // import {sql} from "@vercel/postgres";
 
-// import {getUserById} from "./persistence/user/queries";
+import {getUserById} from "./persistence/user/queries";
 
 export default async function Home() {
   // let {rows} = await sql`SELECT * FROM users`;
-  // let user = await getUserById(1);
-  // let user = null;
-  // if (!user) {
-  //   return <p>Not found</p>;
-  // }
+  let user = await getUserById(1);
+  if (!user) {
+    return <p>Not found</p>;
+  }
 
   return (
     <main>
       <h1>Hello</h1>
-      {/* <p>{user.name}</p>
-      <p>{user.email}</p> */}
+      <p>{user.name}</p>
+      <p>{user.email}</p>
     </main>
   );
 }
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,20 +1,20 @@
-// import postgres from "postgres";
+import postgres from "postgres";
 
-// // TODO make local config
+// TODO make local config
 
-// // let localConfig = {
-// //   user: "postgres",
-// //   host: "localhost",
-// //   port: 5432,
-// // };
-
-// const sql = postgres({
-//   user: process.env.POSTGRES_USER,
-//   host: process.env.POSTGRES_HOST,
+// let localConfig = {
+//   user: "postgres",
+//   host: "localhost",
 //   port: 5432,
-//   database: process.env.POSTGRES_DATABASE,
-//   password: process.env.POSTGRES_PASSWORD,
-//   ssl: true,
-// }); // will use psql environment variables
+// };
 
-// export default sql;
+const sql = postgres({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  port: 5432,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  ssl: true,
+}); // will use psql environment variables
+
+export default sql;
