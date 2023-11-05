0773f48939bc575927a5088aa4c91c62fb5bcf32
masiucd <ciszekmarcell@gmail.com>
2023-11-05 11:47:25 +0100

    SQL test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-export default function Home() {
+import {sql} from "@vercel/postgres";
+
+export default async function Home() {
+  let {rows} = await sql`SELECT * FROM users`;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Hello</h1>
+      <p>{rows[0].name}</p>
+    </main>
   );
 }
