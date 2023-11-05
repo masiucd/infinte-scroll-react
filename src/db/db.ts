376af9d2700b837a6303d747299517fd9fd25c7f
masiucd <ciszekmarcell@gmail.com>
2023-11-05 14:51:13 +0100

    Setup local config

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,20 +1,25 @@
 import postgres from "postgres";
 
-// TODO make local config
+let localConfig = {
+  user: "admin",
+  host: "localhost",
+  port: 5432,
+  database: "postgres",
+  password: "root",
+  ssl: false,
+};
 
-// let localConfig = {
-//   user: "postgres",
-//   host: "localhost",
-//   port: 5432,
-// };
-
-const sql = postgres({
+let prodConfig = {
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   port: 5432,
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
   ssl: true,
-}); // will use psql environment variables
+};
+
+const sql = postgres(
+  process.env.NODE_ENV === "production" ? prodConfig : localConfig
+);
 
 export default sql;
