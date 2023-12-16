14f536c9c76d0233754a270de3f485b99bb1847b
masiucd <ciszekmarcell@gmail.com>
2023-12-16 15:42:30 +0100

    Grouping routes

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 // import {cssBundleHref} from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -27,6 +28,28 @@ export default function App() {
         <Links />
       </head>
       <body className="bg-gray-950 text-gray-50">
+        <header className="min-h-[5rem]">
+          <div className="mx-auto flex max-w-5xl justify-between px-2 py-5">
+            <Link
+              to="/"
+              className="transition-opacity duration-75 hover:opacity-60"
+            >
+              <strong className="text-2xl tracking-tight ">
+                M<span className="text-primary-500">W</span>J
+              </strong>
+            </Link>
+            <nav className="flex">
+              <ul className="flex flex-1 items-center justify-end gap-2 px-2">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/entries/list">Entries</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,13 +11,17 @@ export const meta: MetaFunction = () => [
 
 export default function Index() {
   return (
-    <main className="mx-auto flex min-h-[100dvh] max-w-3xl flex-col border">
+    <main className="mx-auto flex min-h-[100dvh] max-w-3xl flex-col ">
       <h1>My working journal</h1>
       <ul>
         <li>
           <Link to="/entries/list">Entries</Link>
         </li>
       </ul>
+      <p>
+        Here where I journal my progress as a developer. I write about what I
+        learn, what I build, and what I find interesting.
+      </p>
     </main>
   );
 }
